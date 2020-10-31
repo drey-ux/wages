7c5b45de748843562f2c06c020667349825faa94
Damilare <damilareoyetade1@gmail.com>
2020-11-01 00:40:26 +0100

    first commit

--- assets/index.js
+++ assets/index.js
@@ -3,7 +3,7 @@ function solve(){
     var hoursWorked = parseFloat(document.getElementById('workHour').value);
     
     if (hoursWorked > 4){
-        document.getElementById('final').textContent = (4* 1000) + ((hoursWorked - 4) *200);
+        document.getElementById('final').textContent = (hoursWorked* 1000) + ((hoursWorked - 4) *200);
     } else {
         document.getElementById('final').textContent = hoursWorked * 1000;
     }
